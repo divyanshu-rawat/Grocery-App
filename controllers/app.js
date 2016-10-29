9df5d9a7f5e7b907c2315cbca56951fdfe7ef77c
divyanshu <divyanshu.r46956@gmail.com>
2016-10-29 18:44:20 +0530

    added snap_sjots of app git add .

--- controllers/app.js
+++ controllers/app.js
@@ -31,20 +31,19 @@ angular.module('groceryListApp', ["ngRoute"])
 }])
 
 
-.service('GroceryService', [function () {
+.service('GroceryService', [function ($http) {
 
     var grocery_service = [];
 
     grocery_service.grocery_items = [
-        { id: 1 ,completed: true, itemName: 'milk', date: '2014-10-01'},
-        { id: 2 ,completed: true, itemName: 'cookies', date: '2014-10-01'},
-        { id: 3 ,completed: true, itemName: 'ice cream', date: '2014-10-02'},
-        { id: 4 ,completed: true, itemName: 'potatoes', date: '2014-10-02'},
-        { id: 5 ,completed: true, itemName: 'cereal', date: '2014-10-03'},
-        { id: 6 ,completed: true, itemName: 'bread', date: '2014-10-03'},
-        { id: 7 ,completed: true, itemName: 'eggs', date: '2014-10-04'},
-        { id: 8 ,completed: true, itemName: 'tortillas', date: '2014-10-04'}
+        
     ];
+
+    $http('GET|POST|PUT|DELETE', url, post, function(status, response){
+        // success
+    }, function(status, response){
+        // error
+    });
 
     grocery_service.save = function (entry) {
 
