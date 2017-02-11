dae270de05b9bd85a2a25899ac02f6c0b313ab1d
divyanshu <divyanshu.r46956@gmail.com>
2017-02-12 00:13:05 +0530

    added script for avoiding heroku port error !

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ var app = express();
 
 app.use(express.static('public', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 
-app.listen(8000);
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // console.log('Listening on port 3000!');
